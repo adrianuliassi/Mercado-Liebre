dfbba8fc7c3c9b06ca28b6472afc6c0e50423c3a
adrianuliassi <adrianuliassi@hotmail.com>
2022-10-10 20:01:31 -0300

    Subiendo proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+
+const app = express ();
+
+const path = require ('path');
+
+app.get ('/', (req, res) => {
+    res.sendFile (path.join(__dirname,'/views/home.html'))
+});
+
+app.listen (3000, () => 
+console.log ("Servidor corriendo en el puerto 3000"));
+
+app.get('/register', (req,res) => {
+    res.sendFile (path.join(__dirname, '/views/register.html'))
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile (path.join(__dirname, '/views/login.html'))
+});
+
+
+app.use(express.static('public'))
